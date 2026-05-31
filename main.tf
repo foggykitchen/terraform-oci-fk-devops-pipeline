6f58cff05132dee3fd9e346e6b9afd4d60d0eb5e
mlinxfeld <martin.linxfeld@foggykitchen.com>
2026-05-31 17:38:37 +0200

    Avoid build and deploy predecessor cycles

--- main.tf
+++ main.tf
@@ -144,10 +144,10 @@ resource "oci_devops_build_pipeline_stage" "dependent" {
     dynamic "items" {
       for_each = each.value.predecessor_keys
       content {
-        id = coalesce(
-          try(oci_devops_build_pipeline_stage.root["${each.value.pipeline_key}:${items.value}"].id, null),
-          oci_devops_build_pipeline_stage.dependent["${each.value.pipeline_key}:${items.value}"].id
-        )
+        id = contains(
+          keys(local.build_root_stage_map),
+          "${each.value.pipeline_key}:${items.value}"
+        ) ? oci_devops_build_pipeline_stage.root["${each.value.pipeline_key}:${items.value}"].id : oci_devops_build_pipeline_stage.dependent["${each.value.pipeline_key}:${items.value}"].id
       }
     }
   }
@@ -317,10 +317,10 @@ resource "oci_devops_deploy_stage" "dependent" {
     dynamic "items" {
       for_each = each.value.predecessor_keys
       content {
-        id = coalesce(
-          try(oci_devops_deploy_stage.root["${each.value.pipeline_key}:${items.value}"].id, null),
-          oci_devops_deploy_stage.dependent["${each.value.pipeline_key}:${items.value}"].id
-        )
+        id = contains(
+          keys(local.deploy_root_stage_map),
+          "${each.value.pipeline_key}:${items.value}"
+        ) ? oci_devops_deploy_stage.root["${each.value.pipeline_key}:${items.value}"].id : oci_devops_deploy_stage.dependent["${each.value.pipeline_key}:${items.value}"].id
       }
     }
   }
